698d9e83275d1f3843f4e956eb254c61d2056e46
Łukasz Dąbrowski <lukasz.dabrowski.dev@gmail.com>
2018-10-22 21:44:27 +0200

    Main project files

---
+++ SimpleDI.dpr
@@ -0,0 +1,19 @@
+program SimpleDI;
+
+uses
+  Forms,
+  f_Main in 'forms\f_Main.pas' {fMain},
+  f_TestForm in 'forms\f_TestForm.pas' {fTestForm},
+  i_ConfigurationService in 'interfaces\i_ConfigurationService.pas',
+  ConfigurationService in 'services\ConfigurationService.pas',
+  Service in 'services\Service.pas',
+  ServiceManager in 'services\ServiceManager.pas',
+  MockConfigurationService in 'services\MockConfigurationService.pas';
+
+{$R *.RES}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TfMain, fMain);
+  Application.Run;
+end.
